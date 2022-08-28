cfca96107c488f6e28459c7f1ac8df44f2788af1
Ashley Beal <ashleybeal86@yahoo.com>
2022-08-28 14:07:10 -0400

    added events controller and stage controllers, along with blank route template for both, also added events and stage controllers to controllers section in server.js file

---
+++ controllers/events_controller.js
@@ -0,0 +1,29 @@
+// DEPENDENCIES
+const events = require("express").Router();
+const db = require("../models");
+const { Event } = db;
+const { Op } = require("sequelize");
+
+
+// FIND ALL EVENTS--(INDEX ROUTE)
+
+
+
+// FIND A SPECIFIC EVENT--(SHOW ROUTE)
+
+
+
+// CREATE AN EVENT--(CREATE/POST ROUTE)
+
+
+
+// UPDATE AN EVENT--(UPDATE ROUTE)
+
+
+
+//  DELETE AN EVENT--(DELETE ROUTE)
+
+
+
+// EXPORT
+module.exports = events
---
+++ controllers/stages_controller.js
@@ -0,0 +1,29 @@
+// DEPENDENCIES
+const events = require("express").Router();
+const db = require("../models");
+const { Stage } = db;
+const { Op } = require("sequelize");
+
+
+// FIND ALL STAGES--(INDEX ROUTE)
+
+
+
+// FIND A SPECIFIC STAGE--(SHOW ROUTE)
+
+
+
+// CREATE A STAGE--(CREATE/POST ROUTE)
+
+
+
+// UPDATE A STAGE--(UPDATE ROUTE)
+
+
+
+// DELETE A STAGE--(DELETE ROUTE)
+
+
+
+// EXPORT
+module.exports = stages
